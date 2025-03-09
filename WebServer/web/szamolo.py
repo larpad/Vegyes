a67ce7a3473f7906b838634215b2d507665bafe9
larpad <arpad.losonczi@gmail.com>
2025-03-09 17:53:20 +0100

    20250309_1753

--- WebServer/web/szamolo.py
+++
@@ -1,9 +0,0 @@
-class Szamolo:
-    def __init__(self):
-        self.html = "szamolo.html"
-        self.cim = "Egy kis számolás"
-        self.szoveg = "Nos akkor neki esünk a számolásnak..."
-
-    def szamol(self):
-        print("Nyaf")
-    #    return render_template(self.html, cim=self.cim, szoveg=self.szoveg)
